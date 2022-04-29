81f2181dd7671e986295a3b857c9f1af7c4d4b3e
thijsBoet <m.boet@chello.nl>
2022-04-29 14:23:03 +0200

    added letter count and refactored app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const App = () => {
+  const STARTING_TIME = 30;
+  const textareaRef = useRef(null);
+
   const [text, setText] = useState("");
   const [wordCount, setWordCount] = useState(0);
-  const [timeRemaining, setTimeRemaining] = useState(30);
+  const [letterCount, setLetterCount] = useState(0);
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
 
   const handleChange = e => setText(e.target.value);
 
-  const calculateWordCount = (text) => text.trim().split(' ').filter(word => word !== "").length;
+  const calculateWordCount = text =>
+    text
+      .trim()
+      .split(' ')
+      .filter(word => word !== "").length;
+  const calculateLetterCount = text =>
+    text
+      .trim()
+			.split('')
+			.filter(word => word !== '')
+			.filter(word => word !== ' ').length;
+
+  const startGame = () => {
+    setIsTimeRunning(true);
+    setTimeRemaining(STARTING_TIME);
+    setText("");
+    textareaRef.current.disabled = false;
+    textareaRef.current.focus();
+  }
+
+  const endGame = () => {
+    setIsTimeRunning(false);
+		setTimeRemaining(STARTING_TIME);
+  }
 
   useEffect(() => {
-    if (timeRemaining > 0)
-      setTimeout(() => setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 1), 1000)
+		if (timeRemaining > 0 && isTimeRunning) {
+			setTimeout(
+				() => setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 1),
+				1000
+			);
       setWordCount(calculateWordCount(text));
-	}, [timeRemaining]);
+      setLetterCount(calculateLetterCount(text));
+    }
+    if (timeRemaining <= 0) {
+      endGame();
+    }
+	}, [timeRemaining, isTimeRunning]);
 
   return (
 		<div>
@@ -25,10 +61,25 @@ const App = () => {
 				rows='10'
 				value={text}
 				onChange={handleChange}
+				autoFocus
+				disabled={!isTimeRunning}
+				ref={textareaRef}
 			/>
-			<h4>Time remaining {timeRemaining} seconds</h4>
-			<button onClick={handleChange}>Start</button>
-			<h1>Word count: {wordCount === 0 ? '???' : wordCount}</h1>
+			<h4>
+				Time remaining{' '}
+				{timeRemaining === STARTING_TIME
+					? '???'
+					: timeRemaining >= 10
+					? `0:${timeRemaining}`
+					: `0:0${timeRemaining}`}
+			</h4>
+			<button disabled={isTimeRunning} onClick={startGame}>
+				Start
+			</button>
+			<h1>
+				Word count: {wordCount === 0 ? '???' : wordCount} | Letter count:{' '}
+				{letterCount === 0 ? '???' : letterCount}
+			</h1>
 		</div>
 	);
 }
