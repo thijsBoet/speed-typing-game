7d1a5703ebea320b004eee684c828e460dccb6e0
thijsBoet <m.boet@chello.nl>
2022-04-29 12:31:13 +0200

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import React from 'react';
 
 const App = () => {
   return (
     <div>
-
+      <h1>How fast do you type?</h1>
+      <textarea name="" id="" cols="30" rows="10" />
+      <h4>TimeRemaining</h4>
+      <button>Start</button>
+      <h1>WordCount</h1>
     </div>
   );
 }
